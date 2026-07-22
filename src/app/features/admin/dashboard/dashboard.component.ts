566e602cd3cb3fed6a157de0d3868abaa73faa83
Steven72638 <lunasteven282@gmail.com>
2026-07-22 17:37:12 -0500

    refactor(frontend): estandarizacion de componentes y mejoras de UI

--- src/app/features/admin/dashboard/dashboard.component.ts
+++ src/app/features/admin/dashboard/dashboard.component.ts
@@ -7,17 +7,15 @@ import { Router, RouterModule } from '@angular/router';
   selector: 'app-dashboard',
   standalone: true,
   imports: [CommonModule, RouterModule],
-  templateUrl: './dashboard.html',
-  styleUrl: './dashboard.css',
+  templateUrl: './dashboard.component.html',
+  styleUrls: ['./dashboard.component.css'],
 })
 export class Dashboard {
+  private readonly authService = inject(AuthService);
+  private readonly router = inject(Router);
 
-  private AuthService = inject(AuthService);
-  private router = inject(Router);
-
-  logout(){
-
-    this.AuthService.logout();
+  logout() {
+    this.authService.logout();
     this.router.navigate(['/login']);
   }
 }
