7f414dd44adf7353a916ee21a8bcf1a42f8e6dad
Steven72638 <lunasteven282@gmail.com>
2026-07-22 15:19:27 -0500

    style(ui): implementa tema global, rediseño de dashboard y carreras, y GSAP

--- src/app/core/services/carrera/carrera.service.ts
+++ src/app/core/services/carrera/carrera.service.ts
@@ -2,14 +2,14 @@ import { Injectable, inject } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { Carrera, CreateCarreraDto } from '../../models/carrera.model';
-
+import { environment } from '../../../../environments/environment';
 
 @Injectable({
     providedIn: 'root'
 })
 export class CarreraService {
     private readonly http = inject(HttpClient);
-    private readonly apiUrl = 'https://azuaycare-backend.onrender.com/carreras';
+    private readonly apiUrl = `${environment.apiUrl}/carreras`;
 
     getCarreras(): Observable<Carrera[]> {
         return this.http.get<Carrera[]>(this.apiUrl);
@@ -19,12 +19,10 @@ export class CarreraService {
         return this.http.post<Carrera>(this.apiUrl, carrera);
     }
 
-    // Usamos PATCH para actualizar parcialmente o para el borrado lógico (fecha_desactivacion)
     updateCarrera(id: string, carrera: Partial<CreateCarreraDto>): Observable<Carrera> {
         return this.http.patch<Carrera>(`${this.apiUrl}/${id}`, carrera);
     }
 
-    // Borrado físico (si lo permites en tu backend)
     deleteCarrera(id: string): Observable<void> {
         return this.http.delete<void>(`${this.apiUrl}/${id}`);
     }
