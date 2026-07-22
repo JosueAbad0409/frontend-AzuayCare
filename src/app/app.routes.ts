e9a90e1c402029dedfe54719990ad07fecb917cb
Josue Abad <josue.abad.est@tecazuay.edu.ec>
2026-07-22 11:37:52 -0500

    up

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,12 +1,21 @@
 import { Routes } from '@angular/router';
-import { LoginComponent } from './pages/login/login.component';
+import { LoginComponent } from './features/login/login.component';
 import { HomeComponent } from './pages/home/home.component';
 import { AuthGuard } from './core/guards/auth.guard';
 
 export const routes: Routes = [
-  { path: '', redirectTo: 'login', pathMatch: 'full' },
-  { path: 'login', component: LoginComponent },
-  { path: 'home', component: HomeComponent, canActivate: [AuthGuard] },
-  { path: 'admin', component: LoginComponent},
+  {path: '', redirectTo: 'login', pathMatch: 'full'},
+
+  {path: 'login', 
+    loadComponent: () => import('./features/login/login.component').then(m => m.LoginComponent),
+    canActivate: [AuthGuard]
+  },
+
+  {path: 'admin',
+    loadComponent: () => import('./features/admin/dashboard/dashboard').then(m => m.Dashboard),
+    canActivate: [AuthGuard],
+  },
+
   { path: '**', redirectTo: 'login' }
+
 ];
