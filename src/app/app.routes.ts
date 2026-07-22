85c711c02801db330ccb9fb435dd4416bf5bc7eb
Josue Abad <josue.abad.est@tecazuay.edu.ec>
2026-07-22 11:44:32 -0500

    up

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,21 +1,22 @@
 import { Routes } from '@angular/router';
-import { LoginComponent } from './features/login/login.component';
-import { HomeComponent } from './pages/home/home.component';
 import { AuthGuard } from './core/guards/auth.guard';
 
 export const routes: Routes = [
-  {path: '', redirectTo: 'login', pathMatch: 'full'},
+  { path: '', redirectTo: 'login', pathMatch: 'full' },
 
-  {path: 'login', 
-    loadComponent: () => import('./features/login/login.component').then(m => m.LoginComponent),
+  // EL LOGIN NO LLEVA AUTHGUARD (Debe ser público)
+  { 
+    path: 'login', 
+    loadComponent: () => import('./features/login/login.component').then(m => m.LoginComponent)
+  },
+
+  // EL ADMIN SÍ LLEVA AUTHGUARD (Está protegido)
+  { 
+    path: 'admin',
+    loadComponent: () => import('./features/admin/dashboard/dashboard').then(m => m.Dashboard),
     canActivate: [AuthGuard]
   },
 
-  {path: 'admin',
-    loadComponent: () => import('./features/admin/dashboard/dashboard').then(m => m.Dashboard),
-    canActivate: [AuthGuard],
-  },
-
+  // COMODÍN PARA RUTAS NO ENCONTRADAS
   { path: '**', redirectTo: 'login' }
-
 ];
