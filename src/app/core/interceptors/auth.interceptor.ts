ff272abf5e7c601eba33cfecf39bcd79b86bc69c
Josue Abad <josue.abad.est@tecazuay.edu.ec>
2026-07-22 12:56:40 -0500

    up

--- src/app/core/interceptors/auth.interceptor.ts
+++ src/app/core/interceptors/auth.interceptor.ts
@@ -1,19 +1,20 @@
 import { HttpInterceptorFn } from '@angular/common/http';
+import { inject } from '@angular/core';
+import { AuthService } from '../services/auth.service';
 
 export const authInterceptor: HttpInterceptorFn = (req, next) => {
-    // Buscamos el token guardado en el Paso 2
-    const token = localStorage.getItem('token');
+    // Inyectamos tu servicio de autenticación para leer el token
+    const authService = inject(AuthService);
+    const token = authService.token();
 
-    // Si hay token, clonamos la petición y le pegamos el token
+    // Si existe un token, clonamos la petición y le agregamos la cabecera de Autorización
     if (token) {
-        const authReq = req.clone({
-        setHeaders: {
-            Authorization: `Bearer ${token}`
-        }
+        const clonedReq = req.clone({
+            headers: req.headers.set('Authorization', `Bearer ${token}`)
         });
-        return next(authReq);
+        return next(clonedReq); // Mandamos la petición modificada
     }
 
-    // Si no hay token, la petición se va normal
+    // Si no hay token (ej. en el login), mandamos la petición tal como está
     return next(req);
 };
