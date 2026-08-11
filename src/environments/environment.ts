1fd48e419aa16f2f9f0c518405be7c95920c7788
Josue Abad <josue.abad.est@tecazuay.edu.ec>
2026-08-10 20:40:42 -0500

    fix: Update apiUrl to use production backend URL

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  apiUrl: 'http://localhost:3000',
+  apiUrl: 'https://azuaycare-backend.onrender.com',
   googleClientId: '474214477775-m9ci1dg4p6i20s7548et5sfto14750lp.apps.googleusercontent.com'  
 };
